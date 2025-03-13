91460810fcda8d5430380db91863f7e543d9361e
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-03-13 15:51:30 +0000

    fix firebase unable to send req to firebase

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -27,24 +27,83 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Google SignIn'),
+      ),
+      body: _user != null ? _userInfo() : _googleSignInButton(),
+    );
+  }
 
-    Widget _googleSignInButton() {
-      return Center(
-        child: SignInButton(
-          Buttons.google,
-          text: "Sign up using Google",
-          onPressed: () => {},
-        ),
-      );
-    }
+  Widget _googleSignInButton() {
+    return Center(
+      child: SizedBox(
+        height: 50,
+        child: SignInButton(Buttons.google,
+            text: 'Sign up with Google', onPressed: _handleGoogleSignIn,
+            ),
+      ),
+    );
+  }
 
-    Widget _signOutButton() {
-      return Center();
-    }
+  Widget _signOutButton() {
+    return Center();
+  }
 
-    Widget _userInfo(_userInfo) {
-      return SizedBox();
+  Widget _userInfo() {
+    return SizedBox(
+
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Container(
+            // backgroundImage: NetworkImage(_user!.photoURL!),
+            // radius: 50,
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              // shape: BoxShape.circle,
+              image: DecorationImage(
+                image: NetworkImage(_user!.photoURL!),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            'Name: ${_user!.displayName}',
+            style: const TextStyle(fontSize: 20),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            'Email: ${_user!.email}',
+            style: const TextStyle(fontSize: 20),
+          ),
+          MaterialButton(
+            color: Colors.red,
+            child: const Text('Sign Out'),
+            onPressed: _auth.signOut,
+          ),
+        ],
+        //   const SizedBox(height: 10),
+        //   ElevatedButton(
+        //     onPressed: () {}, // _handleSignOut,
+        //     child: const Text('Sign Out'),
+        //   ),
+        // ],
+      ),
+    );
+  }
+
+  void _handleGoogleSignIn() {
+    try {
+      GoogleAuthProvider googleProvider = GoogleAuthProvider();
+      _auth.signInWithProvider(googleProvider);
+    } catch (e) {
+      print(e);
     }
   }
 }
